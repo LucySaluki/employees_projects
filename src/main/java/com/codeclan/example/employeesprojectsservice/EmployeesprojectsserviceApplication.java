51866608044b15136a0bddeb5c7a407777c53959
LucySaluki mslucyarnold@gmail.com <mslucyarnold@gmail.com>
2020-10-06 11:24:11 +0100

    initial commit

---
+++ src/main/java/com/codeclan/example/employeesprojectsservice/EmployeesprojectsserviceApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.employeesprojectsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesprojectsserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesprojectsserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeclan/example/employeesprojectsservice/EmployeesprojectsserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.employeesprojectsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesprojectsserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
